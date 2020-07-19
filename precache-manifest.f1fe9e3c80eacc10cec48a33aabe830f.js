25736e92da20fee0480a66f7f783f0fc8734b1c8
Marcin Stachura <marcin.stachura6@gmail.com>
2020-07-19 15:12:07 +0200

    Updates

---
+++ precache-manifest.f1fe9e3c80eacc10cec48a33aabe830f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b248498a80266ef81c6cebfc81174e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0e6270bb7c2918b26c5",
+    "url": "/static/css/main.1bf71e88.chunk.css"
+  },
+  {
+    "revision": "56ab7f46fb79e61aaa1e",
+    "url": "/static/js/2.ccf67aed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ccf67aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e6270bb7c2918b26c5",
+    "url": "/static/js/main.f6eb9b55.chunk.js"
+  },
+  {
+    "revision": "2b94e1a4fc4635c7be73",
+    "url": "/static/js/runtime-main.2714f617.js"
+  }
+]);
