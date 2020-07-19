6245fbb28cffb1f0b205d3299b87e801a1e94b3e
Marcin Stachura <marcin.stachura6@gmail.com>
2020-07-19 04:29:20 +0200

    changed the code in the way it adds empty element for headerButtons, even if buttons not rendered

--- src/HeaderButtons/index.js
+++ src/HeaderButtons/index.js
@@ -2,20 +2,21 @@ import React from "react";
 import "./style.css";
 
 const HeaderButtons = ({ tasks, hideDoneTasks }) => (
-    tasks.length > 0 && (
-        <span className="headerButtons">
+    <span className="headerButtons">
+        {tasks.length > 0 && (
+            <>
+                <button className="headerButtons__button">
+                    {hideDoneTasks ? "Show " : "Hide "}done tasks
+                </button>
 
-            <button className="headerButtons__button">
-                {hideDoneTasks ? "Show " : "Hide "}done tasks
-            </button>
+                <button
+                    disabled={tasks.every(({ taskDone }) => taskDone)}
+                    className="headerButtons__button">All tasks done!
+                </button>
+            </>
+        )}
+    </span>
 
-            <button
-                disabled={tasks.every(({ taskDone }) => taskDone)}
-                className="headerButtons__button">All tasks done!
-             </button>
-
-        </span>
-    )
 );
 
 export default HeaderButtons;
